29e56947305b40a203c78687eb5dd16f132cba6d
Gokul KR <gokulkr652003@gmail.com>
2026-08-14 16:29:18 +0530

    refactor: wrap app in theme and auth providers

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import HomePage from "./components/HomePage.tsx"
 import CourseDashboard from "./components/CourseDashboard.tsx"
 import LessonPage from "./components/LessonPage.tsx"
 import { CourseProgressProvider } from "./data/progress.tsx"
+import { AuthProvider } from "./data/auth.tsx"
+import { ThemeProvider } from "./data/theme.tsx"
 import "./index.css"
 
 export default function App() {
   return (
-    <Router>
-      <CourseProgressProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/course/:courseId" element={<CourseDashboard />} />
-          <Route
-            path="/course/:courseId/module/:moduleId/lesson/:lessonIndex"
-            element={<LessonPage />}
-          />
-        </Routes>
-      </CourseProgressProvider>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <AuthProvider>
+          <CourseProgressProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/course/:courseId" element={<CourseDashboard />} />
+              <Route
+                path="/course/:courseId/module/:moduleId/lesson/:lessonIndex"
+                element={<LessonPage />}
+              />
+            </Routes>
+          </CourseProgressProvider>
+        </AuthProvider>
+      </Router>
+    </ThemeProvider>
   )
 }
